c1b6efb878517f7fecc702d1dc8bb8859d4ae7cb
Rui Bronze <ruibronze@gmail.com>
2021-05-15 18:58:09 -0300

    Projeto alterado para ver se aparece agora

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -5,6 +5,7 @@ namespace TesteGit {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Teste para cadastro e atualização de dados");
+            Console.WriteLine("Atualização para ver lista de commits no GitHub");
         }
     }
 }
