68a0c850d4426729d7f47aa842070e4b1894e003
Rui Bronze <ruibronze@gmail.com>
2021-05-15 17:44:11 -0300

    Projeto alterado para teste do Git

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -4,6 +4,7 @@ namespace TesteGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste para cadastro e atualização de dados");
         }
     }
 }
